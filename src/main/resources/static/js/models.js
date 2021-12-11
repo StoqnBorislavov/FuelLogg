2b2c3c629af21908f2ed75a742a53c9473deddb2
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-12-11 14:45:48 +0200

    Fix on buisness logic

--- src/main/resources/static/js/models.js
+++ src/main/resources/static/js/models.js
@@ -1,8 +1,8 @@
 let brandHtmlElement = document.querySelector('#brandId');
 brandHtmlElement.addEventListener('change', onChangeHandler);
 
-let baseURL = 'http://localhost:8080/api/model';
-// let baseURL = "https://gentle-temple-45146.herokuapp.com/api/model";
+// let baseURL = 'http://localhost:8080/api/model';
+let baseURL = "https://gentle-temple-45146.herokuapp.com/api/model";
 async function onChangeHandler(e){
     let brandName = e.target.value;
     let response = await fetch(`${baseURL}/${brandName}`);
