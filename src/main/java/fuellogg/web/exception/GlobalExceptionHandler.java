245714f9870794511e4597c82a7bfff1e366258b
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-12-11 13:20:25 +0200

    Exception handling stop

--- src/main/java/fuellogg/web/exception/GlobalExceptionHandler.java
+++ src/main/java/fuellogg/web/exception/GlobalExceptionHandler.java
@@ -12,24 +12,25 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class GlobalExceptionHandler implements ErrorController {
-    private static final String NOT_FOUND = "Page not found!";
-    private static final String DEFAULT_MESSAGE = "Ooops. Something went wrong";
-
-    @RequestMapping("/error")
-    public String handleError(Model model, HttpServletRequest request) {
-        String errorMessage = request.getAttribute(RequestDispatcher.ERROR_MESSAGE).toString();
-        if(errorMessage.equals("")){
-            errorMessage = DEFAULT_MESSAGE;
-        }
-
-        int status = Integer.parseInt(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE).toString());
-        if (status == HttpStatus.NOT_FOUND.value()) {
-            model.addAttribute("errorMessage", NOT_FOUND);
-        } else {
-            model.addAttribute("errorMessage", errorMessage);
-        }
-        return "error";
-    }
+public class GlobalExceptionHandler {
+//     implements ErrorController
+//    private static final String NOT_FOUND = "Page not found!";
+//    private static final String DEFAULT_MESSAGE = "Ooops. Something went wrong";
+//
+//    @RequestMapping("/error")
+//    public String handleError(Model model, HttpServletRequest request) {
+//        String errorMessage = request.getAttribute(RequestDispatcher.ERROR_MESSAGE).toString();
+//        if(errorMessage.equals("")){
+//            errorMessage = DEFAULT_MESSAGE;
+//        }
+//
+//        int status = Integer.parseInt(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE).toString());
+//        if (status == HttpStatus.NOT_FOUND.value()) {
+//            model.addAttribute("errorMessage", NOT_FOUND);
+//        } else {
+//            model.addAttribute("errorMessage", errorMessage);
+//        }
+//        return "error";
+//    }
 }
 
